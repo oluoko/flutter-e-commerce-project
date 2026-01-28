cb2b0c26a71320749d7b12b556cee9c614309235
oluoko <brianotieno586@gmail.com>
2026-01-28 03:49:40 +0300

    Custom tab bar

---
+++ lib/common/widgets/custom_tab_bar.dart
@@ -0,0 +1,32 @@
+import 'package:e_store/utils/constants/colors.dart';
+import 'package:e_store/utils/device_utility.dart';
+import 'package:e_store/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class CustomTabBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomTabBar({
+    super.key,
+    required this.tabs,
+    });
+
+  final List<Widget> tabs;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Material(
+      color: dark ? TColors.black : TColors.white,
+      child: TabBar(
+        tabs: tabs,
+        isScrollable: true,
+        indicatorColor: TColors.primary,
+        labelColor: dark ? TColors.white : TColors.primary,
+        unselectedLabelColor: TColors.darkerGrey ,
+      ),
+    ) ;
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(TDeviceUtils.getAppBarHeight());
+}
--- lib/features/screens/store/store_screen.dart
+++ lib/features/screens/store/store_screen.dart
@@ -2,6 +2,7 @@ import 'package:e_store/common/widgets/brand_title_with_verified_icon.dart';
 import 'package:e_store/common/widgets/cart_counter_icon.dart';
 import 'package:e_store/common/widgets/circular_image.dart';
 import 'package:e_store/common/widgets/custom_app_bar.dart';
+import 'package:e_store/common/widgets/custom_tab_bar.dart';
 import 'package:e_store/common/widgets/grid_layout.dart';
 import 'package:e_store/common/widgets/rounded_container.dart';
 import 'package:e_store/common/widgets/search_container.dart';
@@ -20,105 +21,122 @@ class StoreScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final dark = THelperFunctions.isDarkMode(context);
 
-    return Scaffold(
-      appBar: CustomAppBar(
-        title: Text("Store", style: Theme.of(context).textTheme.headlineMedium),
-        actions: [
-          CartCounterIcon(
-            onPressed: () {},
-            iconColor: dark ? TColors.grey : TColors.darkerGrey,
-          ),
-        ],
-      ),
-      body: NestedScrollView(
-        headerSliverBuilder: (_, innerBoxIsScrollable) {
-          return [
-            SliverAppBar(
-              pinned: true,
-              floating: true,
-              backgroundColor: dark ? TColors.black : TColors.white,
-              expandedHeight: 440,
-              automaticallyImplyLeading: false,
-              flexibleSpace: Padding(
-                padding: EdgeInsets.all(TSizes.defaultSpace),
-                child: ListView(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  children: [
-                    // Search bar
-                    const SizedBox(height: TSizes.spaceBtwItems),
-                    const SearchContainer(
-                      text: "Search our store...",
-                      showBorder: true,
-                      showBackground: false,
-                      padding: EdgeInsets.zero,
-                    ),
-                    const SizedBox(height: TSizes.spaceBtwSections),
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: CustomAppBar(
+          title: Text("Store", style: Theme.of(context).textTheme.headlineMedium),
+          actions: [
+            CartCounterIcon(
+              onPressed: () {},
+              iconColor: dark ? TColors.grey : TColors.darkerGrey,
+            ),
+          ],
+        ),
+        body: NestedScrollView(
+          headerSliverBuilder: (_, innerBoxIsScrollable) {
+            return [
+              SliverAppBar(
+                pinned: true,
+                floating: true,
+                backgroundColor: dark ? TColors.black : TColors.white,
+                expandedHeight: 440,
+                automaticallyImplyLeading: false,
+                flexibleSpace: Padding(
+                  padding: EdgeInsets.all(TSizes.defaultSpace),
+                  child: ListView(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: [
+                      // Search bar
+                      const SizedBox(height: TSizes.spaceBtwItems),
+                      const SearchContainer(
+                        text: "Search our store...",
+                        showBorder: true,
+                        showBackground: false,
+                        padding: EdgeInsets.zero,
+                      ),
+                      const SizedBox(height: TSizes.spaceBtwSections),
 
-                    // Featured Brands
-                    SectionHeading(title: "Featured Brands", onPressed: () {}),
-                    const SizedBox(height: TSizes.spaceBtwItems / 1.5),
+                      // Featured Brands
+                      SectionHeading(title: "Featured Brands", onPressed: () {}),
+                      const SizedBox(height: TSizes.spaceBtwItems / 1.5),
 
-                    GridLayout(
-                      itemCount: 4,
-                      mainAxisExtent: 80,
-                      itemBuilder: (_, index) {
-                        return GestureDetector(
-                          onTap: () {},
-                          child: RoundedContainer(
-                            padding: const EdgeInsets.all(TSizes.sm),
-                            showBorder: true,
-                            backgroundColor: Colors.transparent,
-                            child: Row(
-                              children: [
-                                Flexible(
-                                  child: CircularImage(
-                                    isNetworkImage: false,
-                                    image: TImages.clothIcon,
-                                    backgroundColor: Colors.transparent,
-                                    overlayColor: dark
-                                        ? TColors.white
-                                        : TColors.black,
+                      // Brands Grid
+                      GridLayout(
+                        itemCount: 4,
+                        mainAxisExtent: 80,
+                        itemBuilder: (_, index) {
+                          return GestureDetector(
+                            onTap: () {},
+                            child: RoundedContainer(
+                              padding: const EdgeInsets.all(TSizes.sm),
+                              showBorder: true,
+                              backgroundColor: Colors.transparent,
+                              child: Row(
+                                children: [
+                                  Flexible(
+                                    child: CircularImage(
+                                      isNetworkImage: false,
+                                      image: TImages.clothIcon,
+                                      backgroundColor: Colors.transparent,
+                                      overlayColor: dark
+                                          ? TColors.white
+                                          : TColors.black,
+                                    ),
                                   ),
-                                ),
-                                const SizedBox(width: TSizes.spaceBtwItems / 2),
+                                  const SizedBox(width: TSizes.spaceBtwItems / 2),
 
-                                // Text
-                                Expanded(
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      BrandTitleWithVerificationIcon(
-                                        title: "All Saints",
-                                        brandTextSize: TextSizes.large,
-                                      ),
-                                      Text(
-                                        '231 products',
-                                        overflow: TextOverflow.ellipsis,
-                                        style: Theme.of(
-                                          context,
-                                        ).textTheme.labelMedium,
-                                      ),
-                                    ],
+                                  // Text
+                                  Expanded(
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      crossAxisAlignment:
+                                      CrossAxisAlignment.start,
+                                      children: [
+                                        BrandTitleWithVerificationIcon(
+                                          title: "All Saints",
+                                          brandTextSize: TextSizes.large,
+                                        ),
+                                        Text(
+                                          '231 products',
+                                          overflow: TextOverflow.ellipsis,
+                                          style: Theme.of(
+                                            context,
+                                          ).textTheme.labelMedium,
+                                        ),
+                                      ],
+                                    ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                    ),
+                          );
+                        },
+                      ),
 
-                    // Text
+                      // Text
+                    ],
+                  ),
+                ),
+
+                // Tabs
+                bottom: CustomTabBar(
+                  tabs: [
+                    Tab(child: Text("Sports")),
+                    Tab(child: Text("Clothing")),
+                    Tab(child: Text("Electronics")),
+                    Tab(child: Text("Furniture")),
+                    Tab(child: Text("Cosmetics")),
                   ],
                 ),
               ),
-            ),
-          ];
-        },
-        body: Container(),
+            ];
+          },
+
+          // Body
+          body: Container(),
+        ),
       ),
     );
   }
